9b54c55b14113c6ca1b3a43b9ee75fd207c56d4a
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2024-11-21 11:26:32 +0500

    update

--- a.py
+++ a.py
@@ -1,25 +1,24 @@
-
+import numpy as np
 import json
 grid = [
-    ['p', 0, 0, 0, 'b', 0, 0, 'p'],
-    ['y', 0, 0, 0, 0, 0, 'o', 0],
-    [0, 0, 'b', 0, 'g', 0, 'g', 0],
-    [0, 0, 0, 0, 'l', 0, 'm', 0],
-    [0, 'r', 0, 0, 'r', 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    ['l', 'y', 0, 'o', 'm', 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0,0],
 ]
 
 pairs = {
-    'p': [(0,0),(0,7)],
-    'b':[(0,4),(2,2)],
-    'o':[(1,6),(7,3)],
-    'g':[(2,4),(2,6)],
-    'l':[(3,4),(6,0)],
-    'm':[(3,6),(6,4)],
-    'r':[(4,1),(4,3)],
-    'y':[(0,0),(6,1)]
+    'b': [(0, 7), (6,5)],
+    'r': [(0, 8), (4,3)],
+    'g': [(1, 7), (5,3)],
+    'y': [(0, 0), (6,6)],
+    'o': [(4,5), (7,1)],
+
 }
 
 # sort paris keys accouding to distance
@@ -28,18 +27,6 @@ pairs = dict(sorted(pairs.items(), key=lambda x: abs(x[1][0][0] - x[1][1][0]) + 
 
 initial_state = {color: [pairs[color][0]] for color in pairs}
 overall_history = []
-
-annotaions = {
-    'p':1,
-    'b':2,
-    'o':3,
-    'g':4,
-    'l':5,
-    'm':6,
-    'r':7,
-    'y':8
-
-}
 
 class FlowFreeGame:
 
@@ -85,20 +72,29 @@ class FlowFreeGame:
             # print(f"Color {color} connected successfully.")
             return self.dfs(color_idx + 1, self.pairs[self.colors[color_idx + 1]][0] if color_idx + 1 < len(self.colors) else None)
 
+        # print(self.paths[color])
+
+        
         visited = set(self.paths[color])
+        new_positions = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]]) + np.array([x, y])
+        new_positions_list = new_positions.tolist()
+        new_positions = sorted(new_positions_list, key=lambda x: abs(x[0] - end[0]) + abs(x[1] - end[1]))
 
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            new_position = (x + dx, y + dy)
 
+
+        for new_position in new_positions:
+            new_position = tuple(new_position)
             if new_position in visited:
                 continue
             if self.is_valid_move(new_position, color):
                 self.grid[new_position[0]][new_position[1]] = color
                 self.paths[color].append(new_position)
+                # visited.add(new_position)
                 if self.dfs(color_idx, new_position):
                     return True
                 self.grid[new_position[0]][new_position[1]] = 0
                 self.paths[color].pop()
+                # visited.remove(new_position)
 
         return False
 
@@ -120,6 +116,12 @@ class FlowFreeGame:
     
 
     def solve(self):
+        for color in self.pairs:
+            start, end = self.pairs[color]
+            self.grid[start[0]][start[1]] = color
+            self.grid[end[0]][end[1]] = color
+
+
         if self.dfs(0, self.pairs[self.colors[0]][0]):
             print("Solution found:")
             self.draw_solution(self.grid, self.paths)
